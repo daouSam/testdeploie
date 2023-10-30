7e199006856df2af61d8124d37d59b94e3f366fe
daouSam <sambadaou61@gmail.com>
2023-10-30 08:53:08 +0000

    ok lundi oui encore et encore et encore

--- testdeploie/src/app/app.module.ts
+++ testdeploie/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -126,6 +126,7 @@ const authToken = accessToken;
       multi: true,
     }
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
